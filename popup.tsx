7305be22688f6ae13a2469b928ddbe3ecef9a166
Hritik Sharma <Sharmahritik2002@gmail.com>
2025-01-23 15:09:56 +0530

    chore: remove old icon

--- popup.tsx
+++ popup.tsx
@@ -1,26 +1,15 @@
-import { useState } from "react"
+import { useState } from "react";
 
 function IndexPopup() {
-  const [data, setData] = useState("")
-
   return (
     <div
       style={{
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+        padding: 16,
+      }}
+    >
+      <h2>Welcome to Safarmonk</h2>
     </div>
-  )
+  );
 }
 
-export default IndexPopup
+export default IndexPopup;
